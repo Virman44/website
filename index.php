de2355894ffcb82e4fc8fedcf1ab40c2d626baf0
Virman44 <56293360+Virman44@users.noreply.github.com>
2019-11-24 17:03:42 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,19 +3,33 @@
 include("auth.php");
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
 <meta charset="utf-8">
-<title>Главная страница</title>
-<link rel="stylesheet" href="css/styleindex.css" />
+<title>Фотография и наука</title>
+<link rel="stylesheet" href="css/styleindex1.css" />
 </head>
-<body>
-<div class="form">
-<a id="href1"; href="logout.php">Выйти</a>
-<p id="hello">Добро пожаловать <?php echo $_SESSION['username']; ?>!</p>
-<p id="main">  Смешарики — разумные говорящие существа шарообразной формы, имеющие сходство с различными животными. Среди них нет отрицательных персонажей, если не считать нескольких проходных, появлявшихся в одной-двух сериях. Смешарики населяют Страну Смешариков и живут в Ромашковой долине, отделённой от Большого Мира морем, пустыней, лесом и горами. Смешарики живут дружным коллективом, занимаются различными повседневными делами, попадают в необычные, а иногда и опасные ситуации.
-</p>
-<p><a href="dashboard.php">Смешарики</a></p>
-</div>
+<body background="images/fon1.jpg">
+   <div id="wrapper">   <!--Оболочка-->
+     <div id="header"> 
+     <h3 id="hello"; align="center">Добро пожаловать <?php echo $_SESSION['username']; ?>!</h3>
+       <h1 align="center">Фотография и наука</h1>      
+     </div>
+     <div id="content"> 
+       <h3 align="center">На сайте:</h3> 	 	   <!--меню-->
+	   <p align="center"> <a href="index1.php">История создания фотоизображения.</a> </p>
+		<p align="center"> <a href="index2.php">Физические и химические основы процесса фотографирования.</a></p>
+		<p align="center"> <a href="index3.php">Макро- и микросъемка в научных исследованиях.</a></p>
+        <p align="center"> <a href="index4.php">Пути повышения качества изображения.</a></p>
+		<p align="center"> <a href="index5.php">Интересные факты о фотографии.</a></p>
+		<p align="center"> <a href="index6.php">Как сделать камеру-обскура в домашних условиях.</a></p>
+		<p align="center"> <a href="index7.php">Литература.</a></p>
+</div> 
+  <div class="clear"></div>
+  <div id="footer">
+     <p id="pfooter"; align="center"><a href="logout.php">Выйти</a>
+
+     </div> <!--Запрет наплывания-->
+  
 </body>
 </html>
---
+++ index7.php
@@ -0,0 +1,61 @@
+<?php
+require('db.php');
+include("auth.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Фотография</title>
+        <link rel="stylesheet" href="css/styleindex1.css" />
+</head>
+</head>
+ <body>
+   <div id="wrapper-left">
+   <div id="header">
+    <h3 align="center">Навигация:</h3> 
+  </div>
+          <!--меню-->
+          <div id="content-left"> 
+            <p> <a href="index1.php">История создания фотоизображения.</a></p>
+    <p> <a href="index2.php">Физические и химические основы процесса фотографирования.</a></p>
+    <p> <a href="index3.php">Макро- и микросъемка в научных исследованиях.</a></p>
+        <p> <a href="index4.php">Пути повышения качества изображения.</a></p>
+    <p> <a href="index5.php">Интересные факты о фотографии.</a></p>
+    <p> <a href="index6.php">Как сделать камеру-обскура в домашних условиях.</a></p>
+  </div>
+   <div class="clear"></div> <!--Запрет наплывания-->
+     <div id="footer">
+       <p align="center"><a href="index.php">На главную</a></p>
+<p id="pfooter"; align="center"><a href="logout.php">Выйти</a></p>
+
+     </div>
+  </div>
+   <div id="wrapper">  <!--Оболочка-->
+     <div id="header">  <!--Шапка-->
+       <h2 align="center">Литература</h2>
+        
+     </div>
+     <div id="content"> 
+[1]«Журнал научной и прикладной фотографии и кинематографии», Том 34, Изд-во Академия наук СССР, 1989г.<br>
+[2]Журнал «Наука и жизнь» 03, редакция «Наука и жизнь», 2013г.   <br> 
+[3]«50 эффективных приемов съемки цифровым фотоаппаратом», Кевин Л. Мосс, Изд-во «Вильямс», 2006г.<br>
+[4]http://fotochki.com<br>
+[5]http://kayrosblog.ru<br>
+[6]https://studopedia.ru<br>
+[7]https://ru.wikipedia.org<br>
+[8]http://www.fotoprizer.ru<br>
+[9]http://www.photoisland.net<br>
+[10]https://ribalych.ru<br>
+[11]http://enciklopediya-tehniki.ru
+
+
+
+
+
+     </div>
+  <div class="clear"></div> <!--Запрет наплывания-->
+  
+   </div>
+</body>
+ </html>
